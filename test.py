eddf0bc2d7eac37ee6552088178071c8612974b1
Александра <alexandra918918@gmail.com>
2025-10-16 18:24:51 +0300

    test.py

--- test.py
+++ test.py
@@ -1,32 +1,30 @@
+from laba3 import binary_tree
 import unittest
-from main import gen_bin_tree
+from typing import Dict, List
 
 
-class TestGenBinTree(unittest.TestCase):
-    def test_height_one(self):
-        tree = gen_bin_tree(1, 14)
-        expected = {"value": 14, "left": None, "right": None}
-        self.assertEqual(tree, expected)
+class TestBinaryTree(unittest.TestCase):
 
-    def test_height_two(self):
-        tree = gen_bin_tree(2, 14)
+    def test_height_0(self):
+        result = binary_tree(0, 5, left_leaf=lambda x: x + 1, right_leaf=lambda x: x ** 2)
+        expected = {"5": []}
+        self.assertEqual(result, expected)
+
+    def test_height_1(self):
+        result = binary_tree(1, 5, left_leaf=lambda x: x + 1, right_leaf=lambda x: x ** 2)
+        expected = {"5": [{"6": []}, {"25": []}]}
+        self.assertEqual(result, expected)
+
+    def test_height_2(self):
+        result = binary_tree(2, 5, left_leaf=lambda x: x + 1, right_leaf=lambda x: x ** 2)
         expected = {
-            "value": 14,
-            "left": {"value": -11, "left": None, "right": None},
-            "right": {"value": 28, "left": None, "right": None},
+            "5": [
+                {"6": [{"7": []}, {"36": []}]},
+                {"25": [{"26": []}, {"625": []}]}
+            ]
         }
-        self.assertEqual(tree, expected)
+        self.assertEqual(result, expected)
 
-    def test_height_zero(self):
-        tree = gen_bin_tree(0, 14)
-        self.assertIsNone(tree)
-
-    def test_height_three(self):
-        tree = gen_bin_tree(3, 2)
-        self.assertIsInstance(tree, dict)
-        self.assertEqual(tree["value"], 2)
-        self.assertIn("left", tree)
-        self.assertIn("right", tree)
 
 
 if __name__ == "__main__":
